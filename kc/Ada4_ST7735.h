39c17fbbe6293edfcadd4e42188d2d8c0e942ac6
cryham <cryham@gmail.com>
2018-03-14 22:11:47 +0100

    added Adafruit_GFX with my quick wrapper for ST7735 (using sumotoy code)

---
+++ kc/Ada4_ST7735.h
@@ -0,0 +1,36 @@
+/*********************************************************************
+   Wrapper from ST7735 to Adafruit GFX
+   with double buffering
+*********************************************************************/
+#ifndef _Ada4_ST7735_H_
+#define _Ada4_ST7735_H_
+
+#include <Adafruit_GFX.h>
+
+class Ada4_ST7735 : public Adafruit_GFX
+{
+ public:
+	Ada4_ST7735();
+
+	void begin();
+
+	void clear();
+	void display();
+
+	uint16_t* getBuffer();
+	void drawPixel(int16_t x, int16_t y, uint16_t color);
+
+	void print(const char* str);
+	void print(char c);
+
+//	virtual void drawFastVLine(int16_t x, int16_t y, int16_t h, uint16_t color);
+//	virtual void drawFastHLine(int16_t x, int16_t y, int16_t w, uint16_t color);
+
+private:
+
+	inline void drawFastVLineInternal(int16_t x, int16_t y, int16_t h, uint16_t color) __attribute__((always_inline));
+	inline void drawFastHLineInternal(int16_t x, int16_t y, int16_t w, uint16_t color) __attribute__((always_inline));
+
+};
+
+#endif
