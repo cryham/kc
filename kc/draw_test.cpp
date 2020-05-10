1b92abfaea33ab2b96e4117555464de62968666e
cryham <cryham@gmail.com>
2020-05-10 16:49:53 +0200

    Matrix draw grid, and show held row, col

--- kc/draw_test.cpp
+++ kc/draw_test.cpp
@@ -123,15 +123,27 @@ void Gui::DrawTesting()
 	//-----------------------------------------------------
 	case T_Matrix:
 	{
-		//  matrix  :::
-		const static int16_t x1 = 12, y1 = 64;
-		uint c,r;
+		const static int16_t x1 = 10, y1 = 64;
+		uint c,r, hc=99,hr=99;  //held col,row
+		//const uint X = (W-x1) / (NumCols);
+		const uint X = NumCols < 12 ? 9 : NumCols < 20 ? 8 : 7;
 
+		//  grid #
+		const uint16_t cl[3] = {RGB(9,10,10), RGB(13,14,14), RGB(16,20,20)};
+		int w = NumCols*X;  if (w > W-1-x1) w = W-1-x1;
+		for (r=0; r < NumRows; ++r)
+			if (r % 2 == 1)
+				d->drawFastHLine(x1, y1 + r*8 +4, w, cl[r % 4 == 3 ? 1 : 0]);
+		for (c=0; c < NumCols; ++c)
+			if (c % 2 == 1)
+				d->drawFastVLine(x1 + c*X +3, y1, H-1-y1, cl[c%8==7 ? 2 : c%4==3 ? 1 : 0]);
+
+		//  matrix  :::  *
 		bool ghost = false;
 		for (c=0; c < NumCols; ++c)
 		for (r=0; r < NumRows; ++r)
 		{
-			d->setCursor(x1 + c*8, y1 + r*8);
+			d->setCursor(x1 + c*X, y1 + r*8);
 			const KeyState& k = Matrix_scanArray[NumCols * r + c];
 
 			//  color from ghost, use
@@ -147,6 +159,7 @@ void Gui::DrawTesting()
 				CGh(gh, u);
 				ghost |= gh;
 			}else{
+				hc=c; hr=r;  // held
 				a[0]='*';
 				//sprintf(a,"%d", k.state);
 				d->setClr(24,28,31);
@@ -158,7 +171,7 @@ void Gui::DrawTesting()
 		for (c=0; c < NumCols; ++c)
 		{
 			CGh(col_ghost[c], col_use[c]);
-			d->setCursor(x1 + c*8, y1 - 1*8);
+			d->setCursor(x1 + c*X, y1 - 1*8);
 			sprintf(a,"%d", col_use[c]);
 			d->print(a);
 		}
@@ -173,8 +186,14 @@ void Gui::DrawTesting()
 		//  held  ---
 		d->setCursor(0,26);
 		d->setClr(24,24,31);
-		sprintf(a,"Held %d  press %d", cnt_press-cnt_rel, cnt_press);
+		sprintf(a,"Held %d  press %d ", cnt_press-cnt_rel, cnt_press);
 		d->print(a);
+		if  (hc<99 && hr<99)
+		{
+			d->setCursor(W/2, 6);
+			sprintf(a,"R%d C%d", hr+1, hc+1);
+			d->print(a);
+		}
 
 		//  ghosting  ---
 		d->setCursor(0,40);
